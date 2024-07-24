dc3be578fe6036edcea4184b575281959aa457e5
allaios <co.alla.ios@gmail.com>
2024-07-24 16:14:29 -0700

    Initial Commit

---
+++ Dicee-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee-UIKit
+//
+//  Created by Macbook Pro on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
